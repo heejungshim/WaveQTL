3fc37d012805a151e33a1b72541c9e442937f495
heejungshim <hjshim@gmail.com>
2014-06-20 15:13:34 -0500

    add copyright to python script

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,5 +1,28 @@
 #! /usr/bin/env python
 
+## `extract.py' is python script to extract genotypes and information for a subset of SNPs which are located in a given region.
+##
+## Usage: extract.py CHROMOSOME START END
+##
+## User need to change path to genotype files and informaton files. This script assumes that genotype files are in '/home/shim/wavelets/data/genotype/chrX.YRI.70.mean.genotype.txt' and information files are in '/home/shim/wavelets/data/genotype/chrX.YRI.snpdata.txt'. 
+##
+## for exampe, "./extract.py 17 10158989 10164012" creates `chr17.10158989.10164012.YRI.mean.genotype.txt' and `chr17.10158989.10164012.YRI.snpdata.txt' in the current working directory.
+## 
+## Copyright (C) 2014 Shyam Gopalakrishnan
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 
 if (len(sys.argv) != 4):
@@ -7,8 +30,8 @@ if (len(sys.argv) != 4):
     sys.exit(1)
 
 chrom = sys.argv[1]
-start = int(sys.argv[2]) #- 2000
-end = int(sys.argv[3]) #+ 2000
+start = int(sys.argv[2]) 
+end = int(sys.argv[3]) 
 dirpre = '/home/shim/wavelets/data/genotype/'
 
 ingeno = open(dirpre+'chr'+str(chrom)+'.YRI.70.mean.genotype.txt')
